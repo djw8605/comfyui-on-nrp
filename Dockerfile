4eb8e294148370cefea988f815b79a9830eea939
Derek Weitzel <djw8605@gmail.com>
2025-08-22 23:00:38 -0500

    Change ownership of the files

--- Dockerfile
+++ Dockerfile
@@ -11,10 +11,6 @@ ENV COMFYUI_SESSION_TIMEOUT=600
 ADD jupyter_comfyui_proxy /home/extensions/jupyter_comfyui_proxy
 RUN pip install uv /home/extensions/jupyter_comfyui_proxy/.
 
-# Add comfyui install script
-ADD docker/install_comfyui.sh /usr/local/bin/install_comfyui.sh
-RUN chmod +x /usr/local/bin/install_comfyui.sh
-
 # Configure Jupyter to run comfyui install script at startup
 RUN mkdir -p /usr/local/bin/start-notebook.d
 ADD docker/install_comfyui.sh /usr/local/bin/start-notebook.d/install_comfyui.sh
--- docker/install_comfyui.sh
+++ docker/install_comfyui.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
-COMFYUI_HOME="/home/jovyan/ComfyUI"
+COMFYUI_HOME="/home/$NB_USER/ComfyUI"
 
 
 if [ -d "$COMFYUI_HOME" ]; then
     echo "ComfyUI already installed at $COMFYUI_HOME. Skipping installation."
 else
     echo "Installing ComfyUI to $COMFYUI_HOME..."
-    git clone https://github.com/comfyanonymous/ComfyUI.git "$COMFYUI_HOME"
-    git clone https://github.com/ltdrdata/ComfyUI-Manager "$COMFYUI_HOME/custom_nodes/comfyui-manager"
-    uv pip install --system -r "$COMFYUI_HOME/requirements.txt"
+    sudo -u $NB_USER git clone https://github.com/comfyanonymous/ComfyUI.git "$COMFYUI_HOME"
+    sudo -u $NB_USER git clone https://github.com/ltdrdata/ComfyUI-Manager "$COMFYUI_HOME/custom_nodes/comfyui-manager"
+    sudo -u $NB_USER uv pip install --system -r "$COMFYUI_HOME/requirements.txt"
+    sudo chown -R $NB_USER:$NB_USER "$COMFYUI_HOME"
 fi
